96dd6df0c84ad4612e8918374bd317a2348e5ebe
Suranjit9 <suranjitm07@gmail.com>
2024-03-26 00:30:23 +0600

    install & created Nav Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Heder from '@/components/Heder';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+      <Heder></Heder>
+        {children}
+        </body>
     </html>
   );
 }
